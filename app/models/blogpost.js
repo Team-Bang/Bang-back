5e90baf0772ad64a648c2a7c5c6ee3ba8768dccc
teejaymoo <teajay.m88@gmail.com>
2021-03-23 11:20:15 -0500

    Added blogpost schema and create route Also made a curl-scripts of create for blogpost Alan, Thiago and Carlo collab together on this

---
+++ app/models/blogpost.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const blogPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+module.exports = mongoose.model('BlogPost', blogPostSchema)
---
+++ app/routes/blogpost_routes.js
@@ -0,0 +1,40 @@
+// Express docs: http://expressjs.com/en/api.html
+const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
+const passport = require('passport')
+
+const BlogPost = require('../models/blogpost')
+
+const customErrors = require('../../lib/custom_errors')
+
+const handle404 = customErrors.handle404
+
+const requireOwnership = customErrors.requireOwnership
+
+const removeBlanks = require('../../lib/remove_blank_fields')
+
+const requireToken = passport.authenticate('bearer', { session: false })
+
+const router = express.Router()
+
+// CREATE
+// POST
+router.post('/blogposts', requireToken, (req, res, next) => {
+  // set owner of new example to be current user
+  console.log('The user object:', req.user)
+  console.log('The incoming event data:', req.body)
+  const postData = req.body.blogpost
+  postData.author = req.user._id
+
+  BlogPost.create(postData)
+    // respond to succesful `create` with status 201 and JSON of new "example"
+    .then(blogpost => {
+      res.status(201).json({ blogpost })
+    })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
+    .catch(next)
+})
+
+module.exports = router
