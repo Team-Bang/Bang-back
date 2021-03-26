dc0acede0fab921754ca8ab4ac0343064624e1e7
aqbased <aqbased@hotmail.com>
2021-03-26 08:08:16 -0500

    Add comment schema and route to create a comment
    This comment schema allows user's to reply to other user's blog posts.
    Credit to Thiago, Tj, Carlo and Alan.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const blogpostRoutes = require('./app/routes/blogpost_routes')
+const commentRoutes = require('./app/routes/comment_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -60,7 +61,7 @@ app.use(requestLogger)
 app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(blogpostRoutes)
-
+app.use(commentRoutes)
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
 // passed any error messages from them
