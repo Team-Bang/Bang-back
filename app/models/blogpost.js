ec8eff175097ce2eb208d30499998c16c8da4236
thiagobadini <thiagobardini@hotmail.com>
2021-03-27 10:39:24 -0400

    Add authorName

--- app/models/blogpost.js
+++ app/models/blogpost.js
@@ -10,6 +10,10 @@ const blogPostSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  authorName: {
+    type: String,
+    required: true
+  },
   date: {
     type: Date,
     default: Date.now
